fd474492a4d1e8e05d1e7d8ad59e0e4dd2a32871
MartaM096 <martam096@wp.pl>
2022-09-23 18:41:24 +0200

    added full project

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,44 @@
 import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        String[] happyList=new String[]{"Mateusz","Justyna","Marta","Paulina","Pawel"};
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Give me your name");
+        String name = scanner.nextLine();
+        System.out.println("Give me your surname");
+        String surname = scanner.nextLine();
+        System.out.println("Give me your age");
+        int age = scanner.nextInt();
+
+        if (age>=18) {
+            System.out.println(name + " " + surname + " " + "You can enter");
+            if (age>=18&25>=age) {
+                System.out.println("You have free beer for your age");
+                for(String person:happyList)
+                    if (name.equals(person)) {
+                        System.out.println("Congratulation! Your name is on the happy list. You have also free shot!");
+
+                    }
+            }
+            else {
+                System.out.println("You're to old for free beer");
+            }
+
+            if (age>=30&40>=age) {
+
+                for(String person:happyList)
+                    if (name.equals(person)) {
+                        System.out.println("We want to invate you to VIP room");
+
+                    }
+
+            }
+
+
+        }
+        else{
+            System.out.println(name + " " + surname + " " + "You can't enter");
+        }
+    }
+}
